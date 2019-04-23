cd45ac914d2af2f8ace82a87b8d754f098c2ba5a
Rasmus Kjær Dam <Rallethekid@gmail.com>
2019-04-23 12:07:39 +0200

    Added Unit Test Project

---
+++ StorageBufferTest/TimeregistreringTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StorageBufferTest
+{
+    [TestClass]
+    public class TimeregistreringTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
