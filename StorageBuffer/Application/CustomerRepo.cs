84ecbe1a863a3cb3bd5b3618043b2f624e78d6c6
Zxited <mmgamesfull@gmail.com>
2019-04-24 12:39:37 +0200

    Repo creation

---
+++ StorageBuffer/Application/CustomerRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageBuffer.Domain;
+
+namespace StorageBuffer.Model
+{
+    public class CustomerRepo
+    {
+        private List<Customer> customers;
+
+        public CustomerRepo()
+        {
+            customers = new List<Customer>();
+        }
+    }
+}
---
+++ StorageBuffer/Application/MaterialRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StorageBuffer.Domain;
+
+namespace StorageBuffer.Model
+{
+    public class MaterialRepo
+    {
+        private List<Material> materials;
+
+        public MaterialRepo()
+        {
+            materials = new List<Material>();
+        }
+    }
+}
