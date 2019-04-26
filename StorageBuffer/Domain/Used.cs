19af0fec6b589b42f8aec77412f6aa41def009b7
Zxited <mmgamesfull@gmail.com>
2019-04-26 08:49:52 +0200

    Creates Used Class

---
+++ StorageBuffer/Domain/Used.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageBuffer.Domain
+{
+    public class Used
+    {
+        public Material MaterialObj { get; set; }
+        public Order OrderObj { get; set; }
+        public int Quantity { get; set; }
+        public string Date { get; set; }
+
+        public Used(Material materialObj, Order orderObj, int quantity, string date)
+        {
+            MaterialObj = materialObj;
+            OrderObj = orderObj;
+            Quantity = quantity;
+            Date = date;
+        }
+    }
+}
