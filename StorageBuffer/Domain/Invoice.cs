2f9031e3a9ef8119644fbe5b580f927db8649fc6
Zxited <mmgamesfull@gmail.com>
2019-04-27 21:18:10 +0200

    Created Invoice Class

---
+++ StorageBuffer/Domain/Invoice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageBuffer.Domain
+{
+    public class Invoice
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int OrderId { get; set; }
+        public double Amount { get; set; }
+        public List<Orderline> orderlines;
+
+        public Invoice(int id, int customerId, int orderId, double amount)
+        {
+            Id = id;
+            CustomerId = customerId;
+            OrderId = orderId;
+            Amount = amount;
+            orderlines = new List<Orderline>();
+        }
+    }
+}
