27047abba620a30ba3ea60fec1d57954a1880ef2
Kristian Kastrup Pedersen <kris9139@edu.eal.dk>
2019-04-30 17:31:44 +0200

    change in OrderRepo and CustomerRepo so there are similar in syntax

--- StorageBuffer/Application/CustomerRepo.cs
+++ StorageBuffer/Application/CustomerRepo.cs
@@ -19,13 +19,14 @@ namespace StorageBuffer.Model
         public List<IItem> GetCustomers(string searchQuery)
         {
             List<IItem> result = new List<IItem>();
-                foreach (Customer customer in customers)
+            foreach (Customer customer in customers)
+            {
+                if (customer.Name.ToLower().Contains(searchQuery.ToLower()) || 
+                        customer.Phone.Contains(searchQuery))
                 {
-                    if (customer.Name.ToLower().Contains(searchQuery.ToLower()) || customer.Phone.Contains(searchQuery))
-                    {
                         result.Add(customer);
-                    }
                 }
+            }
 
                 return result;
         }
