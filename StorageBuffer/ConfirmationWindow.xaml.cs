c93bbab51faaf073dff1fe8a66163c0ee2ac0831
Zxited <mmgamesfull@gmail.com>
2019-05-22 16:59:34 +0200

    Created ConfirmationWindow

---
+++ StorageBuffer/ConfirmationWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xaml;
+using StorageBuffer.Domain;
+
+namespace StorageBuffer
+{
+    /// <summary>
+    /// Interaction logic for ChangeOrderline.xaml
+    /// </summary>
+    public partial class ConfirmationWindow : Window
+    {
+        public ConfirmationWindow(string message)
+        {
+            InitializeComponent();
+            lMessage.Content = message;
+        }
+
+        private void BtnConfirm_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+
+        private void BtnDeny_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
