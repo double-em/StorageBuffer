b33aef3ed19252fca13330e04ec2b1b416744a35
Zxited <mmgamesfull@gmail.com>
2019-05-22 08:59:18 +0200

    UpdateCustomer - Unit Test

---
+++ StorageBufferTest/UpdateCustomer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorageBuffer.Application;
+using StorageBuffer.Domain;
+
+namespace StorageBufferTest
+{
+    [TestClass]
+    public class UpdateCustomer
+    {
+
+        private Controller control;
+        private List<Customer> customers;
+        private List<Material> materials;
+        private List<Order> orders;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            control = Controller.Instance;
+            control.GetAllData(DatabaseRepoFakey.Instance);
+
+            customers = control.customerRepo.customers;
+            materials = control.materialRepo.materials;
+            orders = control.orderRepo.orders;
+        }
+
+        [TestMethod]
+        public void CustomerPhoneChange()
+        {
+            string phone = "+4588888888";
+            control.UpdateCustomer(1, "Brian Mariannesen", "Odensevej 24", "5000",
+                "Odense C", phone, "brian.mariannesen@gmail.com", "ingen");
+
+            List<string> customer = control.GetCustomer(1);
+
+            Assert.IsTrue(customer[5] == phone);
+        }
+    }
+}
