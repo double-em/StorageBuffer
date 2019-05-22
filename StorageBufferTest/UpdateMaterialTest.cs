2edd30919f78ce7f7df36142e106a2fae09e2af7
Zxited <mmgamesfull@gmail.com>
2019-05-22 08:54:40 +0200

    UpdateMaterial - Unit Test

---
+++ StorageBufferTest/UpdateMaterialTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StorageBuffer.Application;
+using StorageBuffer.Domain;
+
+namespace StorageBufferTest
+{
+    [TestClass]
+    public class UpdateMaterialTest
+    {
+
+        private Controller control;
+        private List<Customer> customers;
+        private List<Material> materials;
+        private List<Order> orders;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+            control = Controller.Instance;
+            control.GetAllData(DatabaseRepoFakey.Instance);
+
+            customers = control.customerRepo.customers;
+            materials = control.materialRepo.materials;
+            orders = control.orderRepo.orders;
+        }
+
+        [TestMethod]
+        public void MaterialFieldUpdated()
+        {
+            string name = "Bordplade i rustfast";
+            control.UpdateMaterial(11, name, "Ingen", "2");
+            List<string> material = control.GetMaterialLong(11);
+
+            Assert.IsTrue(material[1] == name);
+        }
+    }
+}
